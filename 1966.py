6b8e560122df72107b8e9d6ba9e59dd1eff465b2
minkyojeong <redwonder@naver.com>
2020-03-21 18:10:45 +0900

    1966 문제 수정

--- 1966.py
+++ 1966.py
@@ -1,28 +1,18 @@
-testcase = int(input())
+test_case = int(input())
 
-for i in range(testcase):
+for _ in range(test_case):
     N, M = list(map(int, input().split(' ')))
-    count = 1
-    max_priority = 0
-    queue = []
-    input_value = list(map(int, input().split(' ')))
+    queue = list(map(int, input().split(' ')))
+    queue = [(i, idx) for idx, i in enumerate(queue)]
 
-    for j in range(len(input_value)):
-        queue.append([input_value[j], j])
-        max_priority = max(max_priority, input_value[j])
-
-    while len(queue) != 0:
-        pop_value = queue[0]
-        if pop_value[0] != max_priority:
-            queue.append(pop_value)
-            queue.pop(0)
-        else:
-            if pop_value[1] == M:
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == M:
                 print(count)
                 break
             else:
-                count += 1
                 queue.pop(0)
-                max_priority = 0
-                for value in queue:
-                    max_priority = max(max_priority, value[0])
+        else:
+            queue.append(queue.pop(0))
