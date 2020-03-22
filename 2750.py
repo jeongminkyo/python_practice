576757d4c75f48eaf96bd515f30b9d13d260539b
minkyojeong <redwonder@naver.com>
2020-03-22 17:02:26 +0900

    2750 선택정렬 알고리즘

--- 2750.py
+++ 2750.py
@@ -4,6 +4,13 @@ num_list = []
 for _ in range(N):
     num_list.append(int(input()))
 
-num_list.sort()
+for i in range(N):
+    min_index = i
+    for j in range(i+1, N):
+        if num_list[min_index] > num_list[j]:
+            min_index = j
+
+    num_list[i], num_list[min_index] = num_list[min_index], num_list[i]
+
 for num in num_list:
     print(num)
