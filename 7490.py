22474bf31c51e8f3dda3342146b8e0fee5332e9a
minkyojeong <redwonder@naver.com>
2020-03-26 00:09:55 +0900

    7490 수정

--- 7490.py
+++ 7490.py
@@ -1,35 +1,39 @@
-def cal(num, operation, result):
-    global n
-    global result_set
-    if num == n+1:
-        cal_result = result.replace(' ', '')
-        if eval(cal_result) == 0:
-            result_set.add(result)
+import copy
+
+
+def recursive(array, n):
+    if len(array) == n:
+        operator_list.append(copy.deepcopy(array))
         return
 
-    if operation == '+':
-        result = result + '+' + str(num)
-    elif operation == '-':
-        result = result + '-' + str(num)
-    elif operation == ' ':
-        result = result + ' ' + str(num)
+    array.append(' ')
+    recursive(array, n)
+    array.pop()
 
-    cal(num + 1, '+', result)
-    cal(num + 1, '-', result)
-    cal(num + 1, ' ', result)
+    array.append('+')
+    recursive(array, n)
+    array.pop()
+
+    array.append('-')
+    recursive(array, n)
+    array.pop()
 
 
 test_case = int(input())
-set_list = []
-for _ in range(test_case):
-    result_set = set([])
-    n = int(input())
-    cal(2, '+', '1')
-    cal(2, '-', '1')
-    cal(2, ' ', '1')
-    set_list.append(result_set)
 
-for result_set in set_list:
-    for result in sorted(result_set):
-        print(result)
-    print()
+
+for _ in range(test_case):
+    operator_list = []
+    n = int(input())
+
+    recursive([], n-1)
+
+    integers = [i for i in range(1, n+1)]
+
+    for operators in operator_list:
+        string = ""
+        for i in range(n-1):
+            string += str(integers[i]) + operators[i]
+        string += str(integers[-1])
+        if eval(string.replace(" ","")) == 0:
+            print(string)
